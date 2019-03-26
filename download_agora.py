4f34a121fb271e6994da501eca56e14c090cf6a6
Javy <xujavy@gmail.com>
2019-03-26 13:33:32 +0800

    update download_agro

--- download_agora.py
+++ download_agora.py
@@ -10,8 +10,7 @@ def get_ty_links():
     for i in range(1979, 2017):
         years.append(str(i))
         year_links.append('http://agora.ex.nii.ac.jp/digital-typhoon/year/wnp/'+str(i)+'.html.en')
-    print year_links
-    
+    # print year_links
 
     tys = []
     ty_links = []
@@ -22,7 +21,7 @@ def get_ty_links():
         row0 = soup.find_all(attrs={"class":"ROW0"})
         # get all typhoon-page links
         number = len(row1) + len(row0)
-        print number
+        # print number
         for j in range(1, 10):
             tys.append(years[i] + '0' + str(j))
             ty_links.append('http://agora.ex.nii.ac.jp/digital-typhoon/summary/wnp/k/'+\
@@ -32,7 +31,7 @@ def get_ty_links():
             ty_links.append('http://agora.ex.nii.ac.jp/digital-typhoon/summary/wnp/k/'+\
                             years[i] + str(j) + '.html.en')
     
-    print (tys, ty_links)
+    # print (tys, ty_links)
     return tys, ty_links
 
 def download_imgs(tys, ty_links):
@@ -49,7 +48,7 @@ def download_imgs(tys, ty_links):
         soup = BeautifulSoup(html, "html.parser")
         a_list = soup.find_all('a')
         print '================'
-        print a_list
+        # print a_list
         # all satellite images for every 6 hour
         for a in a_list:
             if a.string != '\n\t\tImage':
@@ -59,8 +58,8 @@ def download_imgs(tys, ty_links):
             html_new = urllib2.urlopen(image_link).read()
             soup_new = BeautifulSoup(html_new,"html.parser")
             tr_list = soup_new.find_all('tr')
-            print '================'
-            print tr_list
+            # print '================'
+            # print tr_list
 
             boo = False
             wind = '0'
@@ -71,7 +70,7 @@ def download_imgs(tys, ty_links):
                         boo = True
                         break
                     wind = str(re.findall(r'\d+',tr_next.string))
-                    print wind
+                    # print wind
             if boo: # 0kt should be excluded
                 continue
 
@@ -80,7 +79,7 @@ def download_imgs(tys, ty_links):
                 if tr.string == '\nCentral Pressure\n':
                     tr_next = tr.next_sibling.next_sibling
                     pressure = str(re.findall(r'\d+',tr_next.string))
-                    print pressure
+                    # print pressure
             
             pict_list = []
             anew_list = soup_new.find_all('a')
@@ -89,23 +88,29 @@ def download_imgs(tys, ty_links):
                     st = anew['href'].replace('/0/','/1/') # replace vis to ir
                     print st
                     pict_list.append('http://agora.ex.nii.ac.jp'+ st)
-                    print pict_list
+            # print '====================='
+            # print pict_list
+            # print '========================'
             
             try: # save images
+                # print pict_list
                 s = pict_list[2]
+                # print s
                 # filename : typhoon-number_time(YYMMDDHH)_wind_pressure.jpg
                 filename = tys[i] + '_' + s[len(s)-19:len(s)-11] + '_' + wind + '_' + pressure
                 filename = rename(filename)
-                print root + filename
-
-                f = open(root + filename + '.jpg', 'wb')
-                req = urllib2.urlopen(s)
-                buf = req.read()
-                f.write(buf)
-                f.close()
+                # print root + filename
+                if os.path.exists(filename):
+                    continue
+                    
+                with open(root + filename + '.jpg', 'wb') as f:
+                    req = urllib2.urlopen(s)
+                    buf = req.read()
+                    f.write(buf)
+                    f.close()
 
             except Exception as e:
-                print(e)
+                print e
 
 	print(tys[i],'has been downloaded.')
 
