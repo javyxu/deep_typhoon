3fb4481e0be37af02ee370de810efb94ad515b77
Javy <xujavy@gmail.com>
2019-03-26 16:14:25 +0800

    update download_agro

--- download_agora.py
+++ download_agora.py
@@ -31,7 +31,7 @@ def get_ty_links():
             ty_links.append('http://agora.ex.nii.ac.jp/digital-typhoon/summary/wnp/k/'+\
                             years[i] + str(j) + '.html.en')
     
-    # print (tys, ty_links)
+    print (tys, ty_links)
     return tys, ty_links
 
 def download_imgs(tys, ty_links):
@@ -43,15 +43,15 @@ def download_imgs(tys, ty_links):
         os.mkdir(root)
 
     for i in range(0, len(ty_links)):
-        print ty_links[i]
+        # print ty_links[i]
         html = urllib2.urlopen(ty_links[i]).read()
         soup = BeautifulSoup(html, "html.parser")
         a_list = soup.find_all('a')
-        print '================'
+        # print '================'
         # print a_list
         # all satellite images for every 6 hour
         for a in a_list:
-            if a.string != '\n\t\tImage':
+            if (str(a.string)).strip() != 'Image':
                 continue
             
             image_link = 'http://agora.ex.nii.ac.jp/'+ a['href']
@@ -64,7 +64,7 @@ def download_imgs(tys, ty_links):
             boo = False
             wind = '0'
             for tr in tr_list:
-                if tr.string == '\nMaximum Wind\n':
+                if (str(tr.string)).strip() == 'Maximum Wind':
                     tr_next = tr.next_sibling.next_sibling
                     if tr_next.string[0] == '0': # 0kt should be excluded
                         boo = True
@@ -76,7 +76,7 @@ def download_imgs(tys, ty_links):
 
             pressure = '1000'
             for tr in tr_list:
-                if tr.string == '\nCentral Pressure\n':
+                if (str(tr.string)).strip() == 'Central Pressure':
                     tr_next = tr.next_sibling.next_sibling
                     pressure = str(re.findall(r'\d+',tr_next.string))
                     # print pressure
@@ -84,9 +84,9 @@ def download_imgs(tys, ty_links):
             pict_list = []
             anew_list = soup_new.find_all('a')
             for anew in anew_list: # find ir images
-                if anew.string == 'Magnify this':
+                if (str(anew.string)).strip() == 'Magnify this':
                     st = anew['href'].replace('/0/','/1/') # replace vis to ir
-                    print st
+                    # print st
                     pict_list.append('http://agora.ex.nii.ac.jp'+ st)
             # print '====================='
             # print pict_list
@@ -94,15 +94,17 @@ def download_imgs(tys, ty_links):
             
             try: # save images
                 # print pict_list
-                s = pict_list[2]
-                # print s
+                # s = pict_list[0].replace('/g/', '/nhc/')
+                # s = pict_list[0]
+                s = pict_list[0].replace('/g/', '/1/')
+                print s
                 # filename : typhoon-number_time(YYMMDDHH)_wind_pressure.jpg
                 filename = tys[i] + '_' + s[len(s)-19:len(s)-11] + '_' + wind + '_' + pressure
                 filename = rename(filename)
                 # print root + filename
                 if os.path.exists(filename):
                     continue
-                    
+
                 with open(root + filename + '.jpg', 'wb') as f:
                     req = urllib2.urlopen(s)
                     buf = req.read()
